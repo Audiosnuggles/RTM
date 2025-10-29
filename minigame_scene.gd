a34ab73867f61c2d8a44976869ef44c4cd8bc6df
Audiosnuggles <moritzfederlein80@gmail.com>
2025-10-29 15:26:20 +0100

    Update minigame_scene.gd

--- minigame_scene.gd
+++ minigame_scene.gd
@@ -1,26 +1,16 @@
-# MinigameScene.gd
-
+# minigame_scene.gd
 extends Node2D
 
-# Dieses Signal wird an die MainScene (die den Level geladen hat) gesendet
 signal minigame_finished(success: bool)
 
-# KEINE _ready() FUNKTION HIER. Das ist wichtig.
-
-# Die Funktion für das Tor (Erfolg)
 func _on_door_body_entered(body):
-	if body.name == "Player":
-		# Erfolg: Szene sofort freigeben
-		minigame_finished.emit(true)
-		queue_free()
+    if body.name == "Player":
+        minigame_finished.emit(true)
+        queue_free()
 
-
-# Methode, die vom Player oder Gegner aufgerufen wird, wenn er stirbt
 func player_died():
-	print("!!! player_died() aufgerufen. Szene wird sofort entfernt. !!!")
-	
-	if get_tree().paused:
-		get_tree().paused = false 
-	
-	minigame_finished.emit(false)
-	queue_free()
+    print("!!! player_died() aufgerufen !!!")
+    if get_tree().paused:
+        get_tree().paused = false 
+    minigame_finished.emit(false)
+    queue_free()
